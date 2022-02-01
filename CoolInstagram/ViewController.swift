3006c678f3c179bfcf1c169ed610361f303c83c4
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2022-02-01 10:45:24 +0200

    Initial Commit

---
+++ CoolInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolInstagram
+//
+//  Created by Vincent Moyo on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
